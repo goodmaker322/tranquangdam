4ed9bccc33b35d72ed3cced69fcdf807b40d9b2b
goodmaker322 <damtqhe163955@fpt.edu.vn>
2025-03-06 00:10:23 +0700

    Updates

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Đảm bảo Tailwind áp dụng trong src
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
